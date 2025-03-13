fb537ea8c07a322840ee8cc296ca406db3f27cc5
MrSeguro <152495876+MrSeguro@users.noreply.github.com>
2025-03-13 15:17:42 +0000

    Search done

--- Search.py
+++ Search.py
@@ -2,6 +2,63 @@ from Graph import Graph
 from WeightEdge import WeightEdge
 from IDNode import IDNode
 
+def GraphCreator(filepath):
+    """
+    Function to create a graph from a given file.
+
+    Requires:
+    filepath: a string with the path to the file
+
+    Ensures:
+    Returns a graph object
+    """
+    graph = Graph()
+    levadasList = []
+
+    # Open and read the file
+    fp = open(filepath, encoding='utf-8')
+    for line in fp.readlines():
+        line = line.strip()
+        line = line.split(', ', 2)
+        if line != ['']:
+            levadasList.append(line)
+    fp.close()
+
+    # Remove the header
+    if levadasList:
+        levadasList.pop(0)
+
+    added_nodes = {}
+    # Add nodes
+    for levada in levadasList:
+        node_id, node_name, edges = levada[0], levada[1], levada[2]
+        if node_id not in added_nodes:
+            new_node = IDNode(node_id, node_name)
+            graph.addNode(new_node)
+            added_nodes[node_id] = new_node
+
+    # Add edges
+    for levada in levadasList:
+        node_id, node_name, edges = levada[0], levada[1], levada[2]
+
+        if edges == '[]':
+            continue
+        
+        edges = edges.strip('[]').split('), (')
+        for edge in edges:
+            edge = edge.strip('()')
+            if ', ' in edge:
+                parts = edge.split(', ')
+                if len(parts) == 2:
+                    dest_id, weight = parts
+                    weight = float(weight)
+                    if dest_id in added_nodes:
+                        source_node = added_nodes[node_id]
+                        dest_node = added_nodes[dest_id]
+                        graph.addEdge(WeightEdge(source_node, dest_node, weight))
+    return graph
+
+
 def getPathWeight(path, graph):
     """
     Requires:
@@ -74,103 +131,23 @@ def printPath(path):
             result += '->'
     return result
 
-def GraphCreator(filepath):
+def testSP():
     """
-    
+    Function to test search in a graph with a specific example
     """
-    graph = Graph()
-    levadasList = []
+    graph = GraphCreator("myLevadasNetworkStor.txt")
+    if graph:
+        print(graph)
+        nodes = list(graph.getNodes())
+        if len(nodes) > 1:
+            start_node = nodes[0]
+            end_node = nodes[-1]
+            paths = search(graph, start_node, end_node)
+            for path, weight in paths:
+                print(f"Path: {printPath(path)} with total weight: {weight}")
+        else:
+            print("Not enough nodes in the graph.")
+    else:
+        print("Failed to create the graph.")
 
-    fp = open(filepath, encoding='utf-8')
-    
-    for line in fp.readlines():
-        line = line.strip()
-        line = line.split(', ', 2)
-
-        if line != ['']:
-            levadasList.append(line)
-
-    levadasList.pop(0)
-    for levada in levadasList:
-        graph.addNode(IDNode(levada[0],levada[1]))
-
-    for levada in levadasList:
-        if len(levada) <= 3:
-            edges = levada[2].strip('[]').split('), (')
-            start_node = IDNode(levada[0], levada[1])
-
-        for edge in edges:
-            edge = edge.strip('()')
-            edge_parts = edge.split(', ')
-            dest_id = str(edge_parts[0]) 
-            weight = int(edge_parts[1])
-
-        
-
-            dest_node = next((node for node in graph._nodes if node.getID() == dest_id), None)
-            if not dest_node:
-                dest_node = IDNode(dest_id, weight)
-                graph.addNode(dest_node)
-
-            graph.addEdge(WeightEdge(start_node, dest_node, weight))
-
-    fp.close()
-
-    return graph
-
-def test_graph():
-    filepath = 'myLevadasNetworkStor.txt'
-    graph = GraphCreator(filepath)
-    
-    # Example of start and end nodes for testing purposes
-    start_node = next(node for node in graph._nodes if node.getName() == 'M' and node.getID() == 0)
-    end_node = next(node for node in graph._nodes if node.getName() == 'PR' and node.getID() == 5)
-    
-    # Find the shortest path
-    sp = search(graph, start_node, end_node)
-    
-    # Print the results
-    print('Shortest path(s) found by DFS:')
-    for path, value in sp:
-        print('Path:', printPath(path), 'Weight:', value)
-
-# Execute the test function
-test_graph()
-
-# def testSP():
-#     """
-#     Function to test search in a graph with a specific example
-#     """
-    
-#     nodes = []
-    
-#     for ID in range(7):  # Create 7 nodes
-#         nodes.append(IDNode(str(ID), str(ID)))  # Agora passamos o ID para o construtor do IDNode
-        
-#     g = Digraph()
-
-#     for n in nodes:
-#         g.addNode(n)
-        
-#     g.addEdge(WeightEdge(nodes[0], nodes[1], 5))
-#     g.addEdge(WeightEdge(nodes[1], nodes[2], 8))
-#     g.addEdge(WeightEdge(nodes[2], nodes[3], 10))
-#     g.addEdge(WeightEdge(nodes[2], nodes[4], 4))
-#     g.addEdge(WeightEdge(nodes[3], nodes[1], 7))
-#     g.addEdge(WeightEdge(nodes[3], nodes[4], 14))
-#     g.addEdge(WeightEdge(nodes[3], nodes[5], 10))
-#     g.addEdge(WeightEdge(nodes[0], nodes[2], 11))
-#     g.addEdge(WeightEdge(nodes[1], nodes[0], 3))
-#     g.addEdge(WeightEdge(nodes[3], nodes[6], 10))
-#     g.addEdge(WeightEdge(nodes[4], nodes[0], 21))
-#     g.addEdge(WeightEdge(nodes[4], nodes[5], 22))
-#     g.addEdge(WeightEdge(nodes[4], nodes[6], 20))
-#     g.addEdge(WeightEdge(nodes[6], nodes[5], 5))
-    
-#     sp = search(g, nodes[0], nodes[5])
-    
-#     print('Shortest path(s) found by DFS:', sp)
-#     for path, weight in sp:
-#         print('Path:', printPath(path), 'Weight:', weight)
-
-# testSP()
+testSP()
